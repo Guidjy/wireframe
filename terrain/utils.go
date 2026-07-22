404f0a833c340c5c416dc66f5976bd5905866c64
Guilherme Pimentel <guippimentel@gmail.com>
2026-07-22 16:11:46 -0300

    removed unused functions

--- terrain/utils.go
+++ terrain/utils.go
@@ -1,8 +1,6 @@
 package terrain
 
 import (
-	"math"
-
 	camera "github.com/Guidjy/wireframe/camera"
 	"github.com/Guidjy/wireframe/config"
 	. "github.com/gen2brain/raylib-go/raylib"
@@ -76,37 +74,4 @@ func DivideColorByFLoat(c1 Color, f float32) Color {
 
 func IsPixelOnScreen(x, y float32) bool {
 	return 0 <= int(x) && int(x) < config.ScreenWidth && 0 <= int(y) && int(y) < config.ScreenHeight
-}
-
-type ColorFloat struct {
-	R, G, B, A float32
-}
-
-func ToColorFloat(c Color) ColorFloat {
-	return ColorFloat{float32(c.R), float32(c.G), float32(c.B), float32(c.A)}
-}
-
-// Safely converts back to uint8, clamping values between 0 and 255 so they never wrap around
-func (cf ColorFloat) ToColor() Color {
-	return Color{
-		R: uint8(math.Min(math.Max(float64(cf.R), 0), 255)),
-		G: uint8(math.Min(math.Max(float64(cf.G), 0), 255)),
-		B: uint8(math.Min(math.Max(float64(cf.B), 0), 255)),
-		A: uint8(math.Min(math.Max(float64(cf.A), 0), 255)),
-	}
-}
-
-func AddColorFloats(c1, c2 ColorFloat) ColorFloat {
-	return ColorFloat{c1.R + c2.R, c1.G + c2.G, c1.B + c2.B, c1.A + c2.A}
-}
-
-func SubtractColorFloats(c1, c2 ColorFloat) ColorFloat {
-	return ColorFloat{c1.R - c2.R, c1.G - c2.G, c1.B - c2.B, c1.A - c2.A}
-}
-
-func DivideColorFloat(c1 ColorFloat, f float32) ColorFloat {
-	if f == 0 {
-		return ColorFloat{}
-	}
-	return ColorFloat{c1.R / f, c1.G / f, c1.B / f, c1.A / f}
 }
